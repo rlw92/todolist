e3844c45f78bd9aa582759d435abff1e976618c4
rlw92 <r_l_williams@yahoo.com>
2022-04-14 15:36:45 +0100

    got the changing quests button down so it changes completed to dos into the done section

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 const actdiv = document.querySelector(".activeQuestContent")
 const compldiv = document.querySelector(".completeQuestContent")
-const quests = [{title:"Wash Hair",
-                 done:false
-                }];
+const quests = [{title:"Wash Hair",done:false},
+                {title:"Brush Teeth",done:false}
+                                ];
 //factory function that  creates quests
 function quest(title,done){
     this.title = title
@@ -15,32 +15,66 @@ function quest(title,done){
   }
 
 //a function that loops through the quests placing them in active or done
-const displayQuests =()=> {
+const displayQuests =(()=> {
+
+const display = () => {
+    actdiv.textContent= "";
+  compldiv.textContent="";
   for(i=0;i<quests.length;i++){
+
+    let dune;
+  
    let div = document.createElement("div");
    let label = document.createElement("label")
    label.textContent = quests[i].title;
    label.setAttribute("id","label"+i)
-   let input = document.createElement("input");
-   input.type = "checkbox";
-   input.setAttribute("id",'inpu'+i);
-   div.appendChild(input);
    div.appendChild(label);
-   if(quests[i].done === true){
-   compldiv.appendChild(div)}
-   else{actdiv.appendChild(div);}
-}} 
-displayQuests();
+      
+  if(quests[i].done === false){
+    dune = false;
+    console.log(dune);
+    let input = document.createElement("div");
+   let button = document.createElement("button");
+   button.addEventListener('click',displayQuests.check);
+   button.textContent = "Done";
+   button.value = i;
+   input.appendChild(button);
+   div.appendChild(input);
+   actdiv.appendChild(div)}
+   else if(quests[i].done===true){compldiv.appendChild(div);}
+  }
+}
+
+const check = (t) => {
+  let tt = t.target.value;
+  console.log(tt);
+  console.log(quests[tt].done);
+  quests[tt].done = true;
+  t.target.style.backgroundColor = "green";
+  displayQuests.display();
+}
+
+  return{display,check}
+
+})();
+  displayQuests.display();
+
+  /*function check(t){
+    let tt = t.target.value;
+    console.log(tt);
+    console.log(quests[tt].done);
+    quests[tt].done = true;
+    t.target.style.backgroundColor = "green";
+    displayQuests.display();
+  }*/
 
 //trying to get the checkbox button to work
-
-let label0 = document.getElementById("label0");
-let inpu0 = document.getElementById("inpu0");
-inpu0.onclick = iop;
- 
-function iop(){
-  if(inpu0.checked === true){document.body.style = "background-color:green;"}
-  else(document.body.style = "background-color:yellow;")
+/*function check(num){
+  if(quests[num].done === false){
+    quests[num].done = true;}
+    else{quests[num].done = false}
+    displayQuests();
 }
-iop();
+
+*/
 
