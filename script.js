f629a86eac039ac7dc2378c4149d7ea81a48b3fa
rlw92 <r_l_williams@yahoo.com>
2022-04-15 14:53:27 +0100

    Trying to understand local storage, figuring out how to implement it into this project.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 const actdiv = document.querySelector(".activeQuestContent")
 const compldiv = document.querySelector(".completeQuestContent")
-const quests = [{title:"Wash Hair",done:false},
+let quests = [{title:"Wash Hair",done:false},
                 {title:"Brush Teeth",done:false}
-                                ];
+                                ];   
 //factory function that  creates quests
 function quest(title,done){
     this.title = title
@@ -18,6 +18,7 @@ const display = () => {
   compldiv.textContent="";
   for(i=0;i<quests.length;i++){
    let div = document.createElement("div");
+
    let label = document.createElement("label")
    label.textContent = quests[i].title;
    label.setAttribute("id","label"+i)
@@ -49,7 +50,7 @@ const check = (t) => {
   displayQuests.display();
 
   
-
+//adding quests into  array using factory function
 const addQuests = () =>{
   let c = prompt("Enter your quest young knight!");
   let title = c;
@@ -57,5 +58,47 @@ const addQuests = () =>{
   let q = new quest(title,done);
   quests.push(q);
   displayQuests.display();
+  localstore.storearray();
 
 }
+
+//storing quests array into local storage
+const localstore = (() =>{
+
+const storearray = ()=>{
+for(i=0;i<quests.length;i++){
+  localStorage.setItem('Questtitle'+ i,quests[i].title);
+}
+}
+
+const printarray = () => {
+  for(i=0;i<quests.length;i++){
+    console.log(localStorage.getItem('Questtitle'+ i));
+  }
+}
+
+const setarray = () =>{
+  for(i=0;i<quests.length;i++){
+    let quests = [{title: localStorage.getItem('Questtitle'+ i),done:false}];
+   };
+  
+
+}
+
+return{storearray,printarray,setarray}
+})();
+localstore.setarray();
+
+
+
+
+
+//testing code to see how local storage works
+document.getElementById("colorch").addEventListener('click',()=>{change()})
+function change(){
+let bgc =  document.getElementById("color").value;
+document.body.style.backgroundColor = bgc;
+localStorage.setItem('backgroundcolor',bgc)}
+
+let loccol = localStorage.getItem('backgroundcolor');
+document.body.style.backgroundColor = loccol;
